ce3f2f487cb7122e71a93b655b55439205bc3074
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-10-04 03:10:20 -0400

    Agregando clasificaciones

--- cisbankServer/tacc/controller/main.js
+++ cisbankServer/tacc/controller/main.js
@@ -2,13 +2,16 @@ const Clasificacion = require("../models/clasification");
 const mainHandler = {
   crearClasificacion: async function (req, res) {
     try {
-        let clasificaciones = req.body
-        let response = [];
-        clasificaciones.forEach(clasificacion => {
-            let aux = new Clasificacion(clasificacion)
-            let data = await aux.save();
-            response.push(data);
-        });
+      let clasificaciones = req.body;
+      let response = [];
+      let length = clasificaciones.length;
+      for (let i = 0; i < length; i++) {
+        const clasificacion = array[i];
+
+        let aux = new Clasificacion(clasificacion);
+        let data = await aux.save();
+        response.push(data);
+      }
       res.status(200).json(response);
     } catch (e) {
       res.status(400).json(e.toString());
@@ -16,8 +19,8 @@ const mainHandler = {
   },
   getClasificaciones: async function (req, res) {
     try {
-        let query = {}
-        let clasificaciones = Clasificacion.find(query);
+      let query = {};
+      let clasificaciones = Clasificacion.find(query);
       res.status(200).json(clasificaciones);
     } catch (e) {
       res.status(400).json(e.toString());
